2c30d333d43c6e7364e6f52c0f50946b4ecc4079
Christian <cnguyen0777@outlook.com>
2021-09-18 01:34:23 -0500

    this has loop that doesn't work as planned

--- revSubEncrypt.cpp
+++ revSubEncrypt.cpp
@@ -15,15 +15,41 @@ string rev(string s){
 }
 
 //Ask for key, shift alpha accordingly, encrypt or decrypt
-
+void key(){
+    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string cipher; 
+    int key;
+    cout << "key: ";
+    cin >> key; 
+    //construct cipher
+    for(int i = 0; i < alpha.size(); ++i){
+        //check if key + i is < 0 or > 26
+        if(key+i > alpha.size()){
+            if(key+i % alpha.size() == 1){
+                cipher += alpha[0];
+            }
+            else{
+                cipher += alpha[key+i % alpha.size()];
+        }
+            } 
+        else if(key+i < 0){
+            cipher += alpha[25 - abs(key+i)];
+        }
+        else{
+            cipher += alpha[key+i];
+        }
+    }
+    cout << cipher;
+}
 
 
 
 int main(){
     string a;
-    cout << "String to reverse: "; //To test rev function 
+    /*cout << "String to reverse: "; //To test rev function 
     getline(cin, a);
     a = rev(a);
-    cout << a; 
+    cout << a;*/
+    key(); 
     return 0; 
 }
