f5442d70316a50ac1421ccc4983ba64407827c1b
Christian <cnguyen0777@outlook.com>
2021-09-26 00:51:43 -0500

    it works

--- revSubEncrypt.cpp
+++ revSubEncrypt.cpp
@@ -1,51 +1,104 @@
 #include <iostream>
-#include <string>
-#include <iterator>
-#include <stdlib.h>
 #include <algorithm>
+#include <string> 
 #include <cmath>
-
-//This program will take an input, reverse it, and apply a substitution cipher
 
 using namespace std; 
 
-//reverse string 
-string rev(string s){
-    reverse(s.begin(), s.end());
-    return s;   
-}
-
-//Ask for key, shift alpha accordingly, encrypt or decrypt
-void key(){
-    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string cipher; 
-    int key;
-    cout << "key: ";
-    cin >> key; 
-    //construct cipher
-    for(int i = 0; i < alpha.length(); ++i){
-        int val = key + i; 
-        if(val > alpha.length()-1){
-            cipher += alpha[val % alpha.length()];
+//encrypt using caesar cipher, then reverse
+string encrypt(string s, int key)
+{
+    //shift elements of s based on key
+    string str = "";
+    if (key > 26 || key < -26)
+    {
+        key = key % 26; 
+    }
+    for (char c : s)
+    {
+        if (isalpha(c))
+        {
+            int upperZDist = 90 - (int)(c);
+            int upperADist = (int)(c) - 65;
+            int lowerZDist = 122 - (int)(c);
+            int lowerADist = (int)(c) - 97; 
+            //for capital letters
+            if (isupper(c))
+            {
+                if ((int)(c) + key > 90)
+                {
+                    str += (char)(64 + (key - upperZDist));
+                }
+                else if ((int)(c) + key < 65)
+                {
+                    str += (char)(91 + (key + upperADist));
+                }
+                else
+                {
+                    str += char(key + (int)(c));
+                }
+            }
+            //for lower letters
+            if (islower(c))
+            {
+                if ((int)(c) + key > 122)
+                {
+                    str += char(96 + (key - lowerZDist));
+                }
+                else if ((int)(c) + key < 97)
+                {
+                    str += char(123 + (key + lowerADist));
+                }
+                else
+                {
+                    str += (key + (int)(c));
+                }
+            }
         }
-        else if(val < 0){
-            cipher += alpha[alpha.length() + val];
-        }
-        else{
-            cipher += alpha[val];
+        else
+        {
+            str += c;
         }
     }
-    cout << cipher;
+    //reverse string before returning 
+    string revStr = "";
+    for(int i = str.length()-1; i >= 0; --i)
+    {
+        revStr += str[i];
+    }
+    return revStr;
 }
 
-
-
-int main(){
-    string a;
-    /*cout << "String to reverse: "; //To test rev function 
-    getline(cin, a);
-    a = rev(a);
-    cout << a;*/
-    key(); 
-    return 0; 
+string decrypt(string s, int key)
+{
+    string decrypt = encrypt(s, -key);
+    return decrypt;
 }
+
+int main()
+{
+    int c;
+    string str;
+    int key;
+    cout << "what do you want to do?\n" << "1:encrypt\n" << "2:decrypt\n";
+    cin >> c;
+    cin.ignore();
+    if (c == 1)
+    {
+        cout << "message to be encrypted: ";
+        getline(cin, str);
+        cout << "key (numeric value): ";
+        cin >> key;
+        cout << encrypt(str, key); 
+    }
+    if (c == 2)
+    {
+        cout << "message to be decrypted: ";
+        getline(cin, str);
+        cout << "key (numeric value): ";
+        cin >> key;
+        cout << decrypt(str, key);
+    }
+    return 0;
+}
+
