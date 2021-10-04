e2cf675d3cdc73ee5663c3e8d2645e67259a2d86
Christian <cnguyen0777@outlook.com>
2021-10-03 19:02:42 -0500

    still wip

--- myEncryption.cpp
+++ myEncryption.cpp
@@ -36,13 +36,22 @@ string encrypt(string s, string key)
     key.append(vowels);
     for (char c : s)
     {
+        int distUpperLim = 126 - (int)(c);
+        int distLowerLim = (int)(c) - 33;
         if (isInKey(c, key))
         {
             if ((int)c+val > 126)
             {
-                
+                eMsg += (char)(33+distUpperLim);
             }
-            eMsg += (char)((int)c+val);
+            else if ((int)c+val < 33)
+            {
+                eMsg += (126 - distLowerLim);
+            }
+            else
+            {
+                eMsg += (char)((int)c+val);
+            }
         }
         else
         {
@@ -52,9 +61,35 @@ string encrypt(string s, string key)
     return eMsg;
 }
 
-void decrypt(string s, string key)
+string decrypt(string s, string key)
 {
-
+    int val = -magicNum(key);
+    key.append(vowels); 
+    string dMsg = "";
+    for (char c : s)
+    {
+        int distUpperLim = 126 - (int)(c);
+        int distLowerLim = (int)(c) - 33;
+        int ch = (int)(c);
+        int cVal = (int)(c) + val; 
+        if (cVal > 126)
+        {
+            cVal = 33 + distUpperLim;
+        }
+        if (cVal < 33)
+        {
+            cVal = 126 - distLowerLim;
+        }
+        if (isInKey((char)cVal, key))
+        {
+            dMsg += (char)cVal; 
+        }
+        else
+        {
+            dMsg += c;
+        }
+    }
+    return dMsg; 
 }
 
 int main()
@@ -65,7 +100,9 @@ int main()
     b.append(vowels);
     cout << b;*/ 
     //cout << magicNum("abc");
-    string a = encrypt("encrypt me!", "cpt");
-    cout << a; 
+    string a = encrypt("christian nguyen", "ngc");
+    cout << "encrypted: " << a << "\n";  
+    string b = decrypt("thrzstzr1 1x*yv1", "ngc");
+    cout << "decrypted: " << b; 
     return 0;
 }
